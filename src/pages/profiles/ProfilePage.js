da5dc744097852d0aaae4766a5e2a3fb61a38909
rkyzk <reikoyazaki@yahoo.com>
2023-07-17 03:38:47 +0000

    display poems of the author on their profile pages using infinit scroll

--- src/pages/profiles/ProfilePage.js
+++ src/pages/profiles/ProfilePage.js
@@ -1,26 +1,31 @@
 import React, { useEffect, useState } from "react";
-import Container from "react-bootstrap/Container";
 
+import Container from "react-bootstrap/Container";
 import appStyles from "../../App.module.css";
 import { useParams } from "react-router";
 import { axiosReq } from "../../api/axiosDefaults";
 import Profile from "./Profile";
 import Poem from "../poems/Poem";
+import InfiniteScroll from "react-infinite-scroll-component";
 import Asset from "../../components/Asset";
+import { fetchMoreData } from "../../utils/utils";
 
 function ProfilePage() {
   const { id } = useParams();
   const [hasLoaded, setHasLoaded] = useState(false);
   const [profileData, setProfileData] = useState({ results: [] });
+  const [profilePoems, setProfilePoems] = useState({ results: [] });
 
   useEffect(() => {
     const handleMount = async () => {
       try {
-        const [{ data: profile }] =
+        const [{ data: profile }, { data: profilePoems }] =
           await Promise.all([
-            axiosReq.get(`/profiles/${id}/`)
+            axiosReq.get(`/profiles/${id}/`),
+            axiosReq.get(`/poems/?owner__profile=${id}`),
           ]);
           setProfileData({ results: [profile] });
+          setProfilePoems(profilePoems);
           setHasLoaded(true);
       } catch (err) {
         console.log(err);
@@ -29,6 +34,29 @@ function ProfilePage() {
     handleMount();
   }, [id]);
 
+  const poems = (
+    <>
+      <h4 className="my-3">Poems by this Writer</h4>
+      {profilePoems.results.length? (
+        <InfiniteScroll
+          children={profilePoems.results.map((poem) => (
+            <Poem
+              key={poem.id}
+              {...poem}
+              setPoems={setProfilePoems}
+            />
+          ))}
+          dataLength={profilePoems.results.length}
+          loader={<Asset spinner />}
+          hasMore={!!profilePoems.next}
+          next={() => fetchMoreData(profilePoems, setProfilePoems)}
+        />
+      ) : (
+        <p>No published poems yet</p>
+      )}
+    </>
+  )
+
   return (
     <Container>
       <Profile
@@ -36,7 +64,7 @@ function ProfilePage() {
         setProfiles={setProfileData}
         profilePage
       />
-      <p>poems by this author</p>
+      {poems}
   </Container> 
   );
 }
