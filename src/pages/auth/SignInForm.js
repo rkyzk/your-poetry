858000357fd8da6d136f9bf91bdc24f3a55f2492
rkyzk <reikoyazaki@yahoo.com>
2023-07-27 15:05:04 +0000

    change style of signin & signup page

--- src/pages/auth/SignInForm.js
+++ src/pages/auth/SignInForm.js
@@ -48,66 +48,64 @@ function SignInForm() {
   };
 
   return (
-    <Row className={styles.Row}>
-      <Col
-        className="my-auto p-0 p-md-2"
-        lg={{ span: 6, offset: 3 }}
-        md={{ span: 8, offset: 2 }}>
-        <Container className={`${appStyles.Content} p-4`}>
-          <h1 className={styles.Header}>sign in</h1>
-          <Form onSubmit={handleSubmit}>
-            <Form.Group controlId="username">
-              <Form.Label className="d-none">Username</Form.Label>
-              <Form.Control
-                className={styles.Input}
-                type="text"
-                placeholder="Username"
-                name="username"
-                value={username}
-                onChange={handleChange}
-              />
-            </Form.Group>
-            {errors.username?.map((message, idx) => (
-              <Alert variant="warning" key={idx}>
-                {message}
-              </Alert>
-            ))}
-            <Form.Group controlId="password">
-              <Form.Label className="d-none">Password</Form.Label>
-              <Form.Control
-                className={styles.Input}
-                type="password"
-                placeholder="Password"
-                name="password"
-                value={password}
-                onChange={handleChange}
-              />
-            </Form.Group>
-            {errors.password?.map((message, idx) => (
-              <Alert variant="warning" key={idx}>
-                {message}
-              </Alert>
-            ))}
-            <Button
-              className={`${btnStyles.Button} ${btnStyles.Wide} ${btnStyles.Bright}`}
-              type="submit"
-            >
-              Sign in
-            </Button>
-            {errors.non_field_errors?.map((message, idx) => (
-              <Alert key={idx} variant="warning" className="mt-3">
-                {message}
-              </Alert>
-            ))}
-          </Form>
-        </Container>
-        <Container className={`mt-3 ${appStyles.Content}`}>
-          <Link className={styles.Link} to="/signup">
-            Don't have an account? <span>Sign up now!</span>
-          </Link>
-        </Container>
-      </Col>
-    </Row>
+    <Col
+      className="my-auto p-0 p-md-2"
+      lg={{ span: 6, offset: 3 }}
+      md={{ span: 8, offset: 2 }}>
+      <Container className="p-4">
+        <h1 className={styles.Header}>sign in</h1>
+        <Form onSubmit={handleSubmit}>
+          <Form.Group controlId="username">
+            <Form.Label className="d-none">Username</Form.Label>
+            <Form.Control
+              className={styles.Input}
+              type="text"
+              placeholder="Username"
+              name="username"
+              value={username}
+              onChange={handleChange}
+            />
+          </Form.Group>
+          {errors.username?.map((message, idx) => (
+            <Alert variant="warning" key={idx}>
+              {message}
+            </Alert>
+          ))}
+          <Form.Group controlId="password">
+            <Form.Label className="d-none">Password</Form.Label>
+            <Form.Control
+              className={styles.Input}
+              type="password"
+              placeholder="Password"
+              name="password"
+              value={password}
+              onChange={handleChange}
+            />
+          </Form.Group>
+          {errors.password?.map((message, idx) => (
+            <Alert variant="warning" key={idx}>
+              {message}
+            </Alert>
+          ))}
+          <Button
+            className={`${btnStyles.Button} ${btnStyles.Large} ${btnStyles.Olive}`}
+            type="submit"
+          >
+            Sign in
+          </Button>
+          {errors.non_field_errors?.map((message, idx) => (
+            <Alert key={idx} variant="warning" className="mt-3">
+              {message}
+            </Alert>
+          ))}
+        </Form>
+      </Container>
+      <Container className="mt-3">
+        <Link className={styles.Link} to="/signup">
+          Don't have an account? <span>Sign up now!</span>
+        </Link>
+      </Container>
+    </Col>
   );
 }
 
--- src/pages/auth/SignUpForm.js
+++ src/pages/auth/SignUpForm.js
@@ -100,7 +100,7 @@ const SignUpForm = () => {
               </Alert>
           ))}
           <Button
-            className={`${btnStyles.Button} ${btnStyles.Wide} ${btnStyles.Bright}`}
+            className={`${btnStyles.Button} ${btnStyles.Large} ${btnStyles.Olive}`}
             type="submit"
           >
             Register
@@ -112,7 +112,7 @@ const SignUpForm = () => {
           ))}
         </Form>
       </Container>
-      <Container className={`mt-3 ${appStyles.Content}`}>
+      <Container className="mt-3">
         <Link className={styles.Link} to="/signin">
           Already have an account? <span>Sign in</span>
         </Link>
